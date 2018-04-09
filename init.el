aa060e7f24e602cd10c4f8d1a021be50f66eba92
Libing Song <libing.song@oracle.com>
2018-04-09 23:15:27 +0800

    Prohibit asking if it should follow the synlink on myinit.org

--- init.el
+++ init.el
@@ -3,7 +3,11 @@
 ;;; Code:
 
 (package-initialize)
+
+;; Don't ask to follow symlinks. Set it as always following.
+(setq vc-follow-symlinks t)
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(setq vc-follow-symlinks 'ask)
 
 (provide 'init)
 ;;; init ends here
